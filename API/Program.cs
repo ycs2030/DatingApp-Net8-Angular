722f6fbd8d8f0d143c352b3f12eab0e7bbf3a0e8
ahmed <acc_ahmed_atef@hotmail.com>
2024-08-06 00:59:12 +0300

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,12 +13,20 @@ builder.Services.AddDbContext<DataContext>(opt =>
     .GetConnectionString("DefaultConnection"));
 });
 
+//add cors policy
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+// adding cors policy
+app.UseCors(op => 
+    op.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200","https://localhost:4200")
+);
 
 
-
+// Configure the HTTP request pipeline.
 app.MapControllers();
 
 app.Run();
